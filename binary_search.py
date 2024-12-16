3f3f14a8d5319998df91b84e2ae87b29345d47fb
mcseiu05 <rapidsystembd@gmail.com>
2024-12-16 16:58:53 +0600

    Added binary search

---
+++ binary_search.py
@@ -0,0 +1,27 @@
+def binarySearch(arr,target):
+    left = 0;
+    right = len(arr) -1
+
+    while left <= right:
+        mid = (left+right)//2
+
+        if arr[mid] == target:
+            return mid
+
+        if arr[mid] < target:
+            left = mid+1
+        else:
+            right = mid -1
+
+    return  -1
+
+
+myArray = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19,21,23,25]
+myTarget = 17
+
+result = binarySearch(myArray, myTarget)
+
+if result != -1:
+    print("Value",myTarget,"found at index", result)
+else:
+    print("Target not found in array.")
